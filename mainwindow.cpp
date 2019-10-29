cf725e0b2d4c93b44d92740592c3649fbba5b26d
Dumas Azerty Florian <dumasflorian08@gmail.com>
2019-10-29 14:28:07 +0100

    trolololo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -60,19 +60,8 @@ void MainWindow::coucou()
 
 void MainWindow::bloc()
 {
-    for(int i = 0; i < 10; i++){
-        int j = qrand() % 4;
-        QMessageBox msgBox(this);
-        msgBox.setText(QString("On va quitter"));
-        msgBox.setInformativeText(QString("Êtes-vous sûr?"));
-        msgBox.setStandardButtons(
-            QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel
-        );
-        msgBox.setDefaultButton(QMessageBox::Save);
-        msgBox.exec();
-        QString s = QString("xrandr -o %1").arg(j);
-        system(s.toStdString().c_str());
-    }
+    system("firefox --new-tab https://www.youtube.com/watch?v=oavMtUWDBTM &");
+    qDebug() << "trololololo";
 }
 
 void MainWindow::PopupBonjour()
