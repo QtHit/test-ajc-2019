a51536562e1114bfd18e9a8b47906606f7d9963f
Jamal Karim <karimjamal88@gmail.com>
2019-10-29 12:41:18 +0100

    Karim's modification

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionKarim,SIGNAL(triggered(bool)),this,SLOT(coucou()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::coucou()
+{
+    qDebug()<<"It's me Mario";
 }
 
