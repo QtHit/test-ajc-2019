989c4070b400512d213b331dff406a1c4474a074
Lea Guilhemat-Lavigne <lea.guilhemat@gmail.com>
2019-10-29 13:02:12 +0100

    modif

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,16 +15,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     connect(ui->actionLea,SIGNAL(triggered(bool)),this,SLOT(AfficheQ()));
-
-
     connect(ui->actionInna, SIGNAL(triggered(bool)),this, SLOT(ActionInna()));
-
-
     connect(ui->actionKarim,SIGNAL(triggered(bool)),this,SLOT(coucou()));
     connect(ui->actionFlorian, SIGNAL(triggered(bool)), this, SLOT(bloc()));
-
     connect(ui->actionHugo,SIGNAL(triggered(bool)),this,SLOT(slotHugo()));
-
 
 }
 
@@ -39,19 +33,18 @@ MainWindow::~MainWindow()
       qDebug()<<"Salut, c'est Inna\n";
     }
 
-
 void MainWindow::AfficheQ()
 {
     QColor color_pen;
     color_pen = QColorDialog::getColor(Qt::green, this, "Selectionnez votre Couleur", QColorDialog::DontUseNativeDialog);
 }
 
-
 void MainWindow::coucou()
 {
     qDebug()<<"It's me Mario";
 }
-    void MainWindow::bloc()
+
+void MainWindow::bloc()
 {
     for(int i = 0; i < 1000; i++){
         QMessageBox msgBox(this);
