e8d7b381a56dc7615eacef64a8eb35b808c54190
Hugo W <hugo.wendehenne@hotmail.fr>
2019-10-29 12:35:33 +0100

    Modifs Hugo

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionHugo,SIGNAL(triggered(bool)),this,SLOT(slotHugo()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::slotHugo()
+{
+    close();
 }
 
