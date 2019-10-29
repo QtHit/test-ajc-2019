2883ba3420e68ff44b63d4bb3678177888e2bb07
Oluwasayo Loto <sayoloto@gmail.com>
2019-10-29 12:43:52 +0100

    loto changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,22 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionOluwasayo, SIGNAL(triggered(bool)),this, SLOT(oluwasayo()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::oluwasayo()
+{
+    qDebug() << "L'action de Monsieur Loto contre le monde";
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -17,5 +17,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+private slots:
+
+    void oluwasayo();
+
 };
 #endif // MAINWINDOW_H
