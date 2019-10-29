17c2417aa6dcb51a665f6d160acf107b8cd1a4b1
Lobanova Inna <inna.i.lobanova@gmail.com>
2019-10-29 12:38:21 +0100

    inna

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "QDebug"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionInna, SIGNAL(triggered(bool)),this, SLOT(ActionInna()));
 }
-
 MainWindow::~MainWindow()
 {
-    delete ui;
-}
+        delete ui;
+    }
+
+    void MainWindow::ActionInna()
+    {
+      qDebug()<<"Salut, c'est Inna\n";
+    }
 
--- mainwindow.h
+++ mainwindow.h
@@ -17,5 +17,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+private slots:
+    void ActionInna();
 };
 #endif // MAINWINDOW_H
